f33017b4b256caa9344885bca8f41aa5e383015b
Zheer Barzan <zheerbarzan85@outlook.com>
2023-11-23 19:47:52 +0300

    making the first version of the program

--- Lab_1/Question_2/src/Main.java
+++ Lab_1/Question_2/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is the value of A?");
+        int a = input.nextInt();
+        System.out.println("what is the value of B?");
+        int b = input.nextInt();
+        System.out.println("what is the value of C?");
+        int c= input.nextInt();
+        int x = quadraticFunction(a,b,c);
+
+        System.out.println("the value of X = "+x);
+    }
+
+    public static int quadraticFunction(int a,int b, int c){
+
+        int delta= (int) (Math.pow(b,2)- 4*a*c);
+        int x = (int) ((-(b+Math.sqrt(delta)))/2*a)/100;
+
+        return x;
     }
 }
