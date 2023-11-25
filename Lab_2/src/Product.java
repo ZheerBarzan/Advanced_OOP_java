d443177e92286b484220e0c58cea798271d7f496
Zheer Barzan <zheerbarzan85@outlook.com>
2023-11-25 18:16:37 +0300

    making the construstor and the variables for the product class

--- Lab_2/src/Product.java
+++ Lab_2/src/Product.java
@@ -1,3 +1,16 @@
 public class Product {
+    private int product_id;
+    private String product_name;
+    private double product_price;
+    private int product_year;
+    private int product_count;
+
+    public Product(int id, String name, double price, int year, int count) {
+        this.product_id = id;
+        this.product_name = name;
+        this.product_price = price;
+        this.product_year = year;
+        this.product_count = count;
+    }
 
 }
