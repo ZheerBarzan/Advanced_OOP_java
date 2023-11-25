0a301148985b6efc6580db0997b153bb7adfe943
Zheer Barzan <zheerbarzan85@outlook.com>
2023-11-25 18:33:10 +0300

    done with the layout of the methods for the product class

--- Lab_2/src/Product.java
+++ Lab_2/src/Product.java
@@ -1,4 +1,6 @@
 public class Product {
+
+    private final int DISCOUNT_RATE = 5;
     private int product_id;
     private String product_name;
     private double product_price;
@@ -12,5 +14,65 @@ public class Product {
         this.product_year = year;
         this.product_count = count;
     }
+    // make another constructor with 3 values(id,name,price) year by defualt is 2023 and count by defualt is 100
 
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public double getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(double product_price) {
+        this.product_price = product_price;
+    }
+
+    public int getProduct_year() {
+        return product_year;
+    }
+
+    public void setProduct_year(int product_year) {
+        this.product_year = product_year;
+    }
+
+    public int getProduct_count() {
+        return product_count;
+    }
+
+    public void setProduct_count(int product_count) {
+        this.product_count = product_count;
+    }
+
+    //format string with product id,product name and product selling price
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    //after applying the discount it computes the selling price
+    public double computeSellingPrice(){
+        return 0;
+    }
+    //it deducts a number from the product count
+    public int soldNumber(int sold){
+
+        return sold;
+    }
+    // it computes the discount value using the discount_rate
+    private double computeDiscount(){
+
+        return 0;
+    }
 }
