79a837bb292f83690f701341c63941511895c612
Zheer Barzan <zheerbarzan85@outlook.com>
2023-11-28 23:08:27 +0300

    adding the constructer and getters and setters to the course class

--- Lab_3/src/Course.java
+++ Lab_3/src/Course.java
@@ -5,4 +5,51 @@ public class Course {
     private String venue;
     private double mark;
 
+    public Course(String title, String day, String time, String venue, double mark) {
+        this.title = title;
+        this.day = day;
+        this.time = time;
+        this.venue = venue;
+        this.mark = mark;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
 }
