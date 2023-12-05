c59ff793846e8331ad8b2bbb1d878869ba5421c6
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-05 15:01:36 +0300

    done with the Circle class and its methods

--- Lab_4/src/Circle.java
+++ Lab_4/src/Circle.java
@@ -1,2 +1,40 @@
-public class Circle {
+public class Circle extends Shape {
+
+    protected double radius;
+
+    public Circle(){
+
+    }
+    public Circle(double radius){
+        this.radius= radius;
+
+    }
+    public Circle(double radius,String color,boolean filled){
+        this.radius=radius;
+        this.color = color;
+        this.filled= filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
