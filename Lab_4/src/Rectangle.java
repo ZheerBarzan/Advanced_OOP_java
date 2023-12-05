86d3af17bbebf9bb9f710b09d9444d4583987a19
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-05 15:07:05 +0300

    done with the rectangle class and its methods

--- Lab_4/src/Rectangle.java
+++ Lab_4/src/Rectangle.java
@@ -1,2 +1,50 @@
-public class Rectangle {
+public class Rectangle extends Shape {
+    protected double width;
+    protected double length;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+    public Rectangle(double width, double length, String color, boolean filled){
+        this.width = width;
+        this.length = length;
+        this.color = color;
+        this.filled = filled;
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- Lab_4/src/Square.java
+++ Lab_4/src/Square.java
@@ -1,4 +1,4 @@
-public class Square extends Rectangle{
+public class Square extends Rectangle {
 
 
 
