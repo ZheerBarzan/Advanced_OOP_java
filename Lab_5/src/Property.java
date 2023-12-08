17e1726098bb4cfaacadd1827cc7d1bbfccaa6db
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-08 17:26:40 +0300

    adding the constructors

--- Lab_5/src/Property.java
+++ Lab_5/src/Property.java
@@ -3,7 +3,22 @@ public class Property {
     private double area;
     private String city;
     private char type;
-    private int Year;
+    private int year;
     private int price;
     private String owner;
+
+    public Property() {
+    }
+
+    public Property(int id, double area, String city, char type, int year, int price, String owner) {
+        this.id = id;
+        this.area = area;
+        this.city = city;
+        this.type = type;
+        this.year = year;
+        this.price = price;
+        this.owner = owner;
+    }
+
 }
+
