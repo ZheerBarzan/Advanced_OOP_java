b309e8031dbaa881f514d893c754286e6962c3b3
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-08 17:25:05 +0300

    adding all the necessary info for the property class

---
+++ Lab_5/src/Property.java
@@ -0,0 +1,9 @@
+public class Property {
+    private int id;
+    private double area;
+    private String city;
+    private char type;
+    private int Year;
+    private int price;
+    private String owner;
+}
