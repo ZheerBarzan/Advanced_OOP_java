12dbd758e481d178e7bad087e3216651338623e1
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-22 18:00:17 +0300

    some changes to the lab 4

--- Lab_4/src/Rectangle.java
+++ Lab_4/src/Rectangle.java
@@ -10,10 +10,9 @@ public class Rectangle extends Shape {
         this.length = length;
     }
     public Rectangle(double width, double length, String color, boolean filled){
+        super(color,filled);
         this.width = width;
         this.length = length;
-        super.color = color;
-        super.filled = filled;
 
     }
 
