cbb70a4cf82cb7be3b9ef90529571e7d30ff2620
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-30 17:29:58 +0300

    adding the subject class of the program

--- Design_patterns/Observer pattern/src/WeatherData.java
+++ Design_patterns/Observer pattern/src/WeatherData.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
+
 public class WeatherData {
 
+private ArrayList<Observer> views;
+float tempretrue = 0;
+float humidty =0;
 
+public void registerData(Observer observer){
+    views.add(observer);
+}
+public void removeData(Observer observer){
+    views.remove(observer);
+}
+public void notifyObservers(){
+    for (Observer observer: views){
+        observer.update();
+    }
+}
+
+    public float getTempretrue() {
+        return tempretrue;
+    }
+
+    public void setTempretrue(float tempretrue) {
+        this.tempretrue = tempretrue;
+notifyObservers();    }
+
+    public float getHumidty() {
+        return humidty;
+    }
+
+    public void setHumidty(float humidty) {
+        this.humidty = humidty;
+notifyObservers();    }
 }
