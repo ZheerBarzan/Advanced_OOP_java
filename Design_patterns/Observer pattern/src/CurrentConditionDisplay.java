d927a28ab444436e8a67da258aa4fa66c33fd366
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-30 17:40:16 +0300

    adding the interface methods to the observer 1

--- Design_patterns/Observer pattern/src/CurrentConditionDisplay.java
+++ Design_patterns/Observer pattern/src/CurrentConditionDisplay.java
@@ -1,2 +1,14 @@
-public class CurrentConditionDisplay  {
+public class CurrentConditionDisplay  implements Observer{
+
+    private WeatherData data;
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        data = weatherData;
+        data.registerData(this);
+    }
+
+    @Override
+    public void update(float temp, float humidity) {
+        System.out.println("Observer_1 { temp: "+data.getTempretrue()+",\n humdity: "+data.getHumidty() );
+    }
 }
