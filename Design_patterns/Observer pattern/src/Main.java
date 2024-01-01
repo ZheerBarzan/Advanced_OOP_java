95fb23c66dc991551e9a26b5c81c50d0fbdedf23
Zheer Barzan <zheerbarzan85@outlook.com>
2024-01-01 14:53:58 +0300

    creating the necessary objects

--- Design_patterns/Observer pattern/src/Main.java
+++ Design_patterns/Observer pattern/src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+
 
     }
 }
