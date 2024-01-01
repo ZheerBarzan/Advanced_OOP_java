e69d3fbae336c528b0eeaf50c8f62f758541c016
Zheer Barzan <zheerbarzan85@outlook.com>
2024-01-01 14:48:59 +0300

    creating the constructor with the weatherData parameter

--- Design_patterns/Observer pattern/src/StatisticsDisplay.java
+++ Design_patterns/Observer pattern/src/StatisticsDisplay.java
@@ -1,6 +1,10 @@
 public class StatisticsDisplay implements Observer{
     private WeatherData data;
 
+    public StatisticsDisplay(WeatherData weatherData) {
+        data = weatherData;
+        data.registerData(this);
+    }
 
     @Override
     public void update(float temp, float humidity) {
