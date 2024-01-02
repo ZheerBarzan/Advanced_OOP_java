26c7ad0abfed2450f54722f04538615ab1b4b26c
Zheer Barzan <zheerbarzan85@outlook.com>
2024-01-02 16:10:16 +0300

    adding the methods to the interface

--- RMI/src/CalculatorInterface.java
+++ RMI/src/CalculatorInterface.java
@@ -2,5 +2,13 @@ import java.rmi.*;
 
 public interface CalculatorInterface extends Remote{
 
+public int add(int x, int y) throws RemoteException;
+public int sub(int x, int y) throws RemoteException;
+
+public int mul(int x, int y) throws RemoteException;
+
+public int div(int x, int y) throws RemoteException;
+
+
 
 }
