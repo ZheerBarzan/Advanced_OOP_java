9b3e8a0497d1d2a9522cf16de6d525bbca952e34
Zheer Barzan <zheerbarzan85@outlook.com>
2024-01-08 15:49:05 +0300

    combining the GUI with the client

--- RMI/src/CalculatorClient.java
+++ RMI/src/CalculatorClient.java
@@ -1,11 +1,41 @@
+import javax.swing.*;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-public class CalculatorClient {
+public class CalculatorClient extends JFrame{
+    public CalculatorClient(){
+
+
+
+
+
+        setTitle("RMI Calculator APP");
+        setSize(300,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+
+        JLabel firstNumLabel = new JLabel("Enter the first number");
+        panel.add(firstNumLabel);
+
+        JTextField firstNumField = new JTextField();
+        panel.add(firstNumField);
+
+        JLabel secondNumLabel = new JLabel("Enter the second number");
+        panel.add(secondNumLabel);
+
+        JTextField secondNumField = new JTextField();
+        panel.add(secondNumField);
+        setContentPane(panel);
+        setVisible(true);
+
+    }
 
     public static void main(String[] args) {
+        new CalculatorClient();
         try{
             CalculatorInterface stub = (CalculatorInterface) Naming.lookup("rmi://localhost/Cal");
             System.out.println("the addition: "+stub.add(3,4));
